2e8035dc22d8090f4ed7b375b2de1d3979614c8f
Esirn <31148342+Esirn@users.noreply.github.com>
2019-03-09 13:01:00 +0800

    Bigbang

---
+++ p2_1.py
@@ -0,0 +1,16 @@
+import random
+
+answer = random.randint(1,10)
+temp = input("guess a number:")
+guess = int(temp)
+if guess != answer:
+    while guess!=answer:
+        if guess <answer:
+            print("Too small!")
+        else:
+            print("Too big!")
+        temp = input("guess a number:")
+        guess = int(temp)
+    print("Bingo!")
+else:
+    print("Bingo!")
---
+++ p4_4.py
@@ -0,0 +1,9 @@
+bingo = 18
+answer = int(input("guess the number"))
+while 1:
+    if answer == bingo:
+        break
+    else:
+        answer = int(input("guess again"))
+print("cool.")
+        
